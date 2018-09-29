3f7bb7f5292763a1c86e0ec83ad3b45a140f0295
k <>
2018-09-29 00:01:22 -0700

    added igraph package

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     name="neuraltree",
     packages=find_packages(exclude=["test"]),
     include_package_data=True,
-    setup_requires=["Cython"],
     install_requires=[
         "Cython",
         "Keras",
         "pytest",
+        # TODO: implement custom install command to install the igraph C core
+        "python-igraph",
         "tensorflow"
     ],
     test_suite="test"
